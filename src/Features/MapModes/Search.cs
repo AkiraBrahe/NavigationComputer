209ff754325d0021e2e1366716bdc65308061cfd
Kierk <136152906+AkiraBrahe@users.noreply.github.com>
2025-10-31 13:56:22 +0100

    Reorder searchable tags

--- src/Features/MapModes/Search.cs
+++ src/Features/MapModes/Search.cs
@@ -12,29 +12,16 @@ namespace NavigationComputer.Features.MapModes
     {
         private static readonly Dictionary<string, string> TagIdToFriendlyName = new()
         {
-            {"planet_industry_agriculture", "agriculture"},
-            {"planet_industry_aquaculture", "aquaculture"},
-            {"planet_industry_manufacturing", "manufacturing"},
-            {"planet_industry_mining", "mining"},
-            {"planet_industry_poor", "poor"},
-            {"planet_industry_recreation", "recreation"},
-            {"planet_industry_research", "research"},
-            {"planet_industry_rich", "rich"},
-
             {"planet_size_large", "high gravity planet"},
             {"planet_size_medium", "medium gravity planet"},
             {"planet_size_small", "low gravity planet"},
-
-            {"planet_civ_innersphere", "inner sphere-level civilization"},
-            {"planet_civ_periphery", "periphery-level civilization"},
-            {"planet_civ_primitive", "primitive civilization"},
 
             {"planet_climate_arctic", "arctic world"},
             {"planet_climate_arid", "arid world"},
             {"planet_climate_desert", "desert world"},
             {"planet_climate_ice", "ice world"},
-            {"planet_climate_mars", "martian world"},
             {"planet_climate_lunar", "lunar world"},
+            {"planet_climate_mars", "martian world"},
             {"planet_climate_moon", "barren world"},
             {"planet_climate_rocky", "rocky world"},
             {"planet_climate_terran", "terran world"},
@@ -47,13 +34,16 @@ namespace NavigationComputer.Features.MapModes
             {"planet_other_boreholes", "geothermal boreholes"},
             {"planet_other_capital", "regional capital"},
             {"planet_other_comstar", "comstar presence"},
+            {"planet_other_dragons", "native dragons"},
             {"planet_other_empty", "uninhabited"},
+            {"planet_other_factionhq", "factory world"},
             {"planet_other_floatingworld", "dense cloud layer"},
             {"planet_other_fungus", "dominant fungus"},
             {"planet_other_gasgiant", "gas giant moon"},
             {"planet_other_hub", "travel hub"},
             {"planet_other_megacity", "megacity"},
             {"planet_other_megaforest", "planetwide forest"},
+            {"planet_other_moon", "moons"},
             {"planet_other_mudflats", "planetwide mudflats"},
             {"planet_other_newcolony", "recently colonized"},
             {"planet_other_pirate", "pirate presence"},
@@ -65,12 +55,29 @@ namespace NavigationComputer.Features.MapModes
             {"planet_other_storms", "planetwide storms"},
             {"planet_other_taintedair", "tainted atmosphere"},
             {"planet_other_volcanic", "extensive vulcanism"},
-            {"planet_other_moon", "moons"},
 
             {"planet_pop_large", "large population"},
             {"planet_pop_medium", "moderate population"},
             {"planet_pop_none", "token population"},
-            {"planet_pop_small", "small population"}
+            {"planet_pop_small", "small population"},
+
+            {"planet_civ_innersphere", "inner sphere-level civilization"},
+            {"planet_civ_periphery", "periphery-level civilization"},
+            {"planet_civ_primitive", "primitive civilization"},
+
+            {"planet_industry_rich", "rich"},
+            {"planet_industry_poor", "poor"},
+
+            {"planet_industry_agriculture", "agriculture"},
+            {"planet_industry_aquaculture", "aquaculture"},
+            {"planet_industry_chemicals", "chemicals"},
+            {"planet_industry_electronics", "electronics"},
+            {"planet_industry_manufacturing", "manufacturing"},
+            {"planet_industry_mining", "mining"},
+            {"planet_industry_recreation", "recreation"},
+            {"planet_industry_research", "research"},
+
+            {"planet_faction_independent", "independent"}
         };
 
         private readonly float _dimLevel = dimLevel;
