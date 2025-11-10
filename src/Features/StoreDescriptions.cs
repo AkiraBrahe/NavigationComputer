a80972e85592554dde59ab01cb284e9d4d9fd1a4
Kierk <136152906+AkiraBrahe@users.noreply.github.com>
2025-11-11 00:03:53 +0100

    Add factory system descriptions

---
+++ src/Features/StoreDescriptions.cs
@@ -0,0 +1,186 @@
+﻿using BattleTech;
+using BEXTimeline;
+using NavigationComputer.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavigationComputer.Features
+{
+    internal class StoreDescriptions
+    {
+        public static DateTime LastYearUpdated = new(1999, 1, 1);
+
+        /// <summary>
+        ///  Updates factory system descriptions to include all the mechs and vehicles produced at this system.
+        /// </summary>
+        [HarmonyPatch(typeof(UpdateOwnership), "UpdateTheMap")]
+        public static class UpdateOwnership_UpdateTheMap
+        {
+            [HarmonyPostfix]
+            public static void Postfix(SimGameState simGame)
+            {
+                if (simGame.CurrentDate.Year <= LastYearUpdated.Year)
+                    return;
+
+                LastYearUpdated = new DateTime(simGame.CurrentDate.Year, 1, 1);
+
+                foreach (var systemList in simGame.FactionStoreStarSystemsDictionary.Values)
+                {
+                    foreach (var system in systemList)
+                    {
+                        if (system.Def.Description.Details.Contains("<b>LOCAL PRODUCTION:</b>"))
+                            continue;
+
+                        var factoryItemCollections = GetFactoryItemCollections(simGame, system);
+                        if (factoryItemCollections.Count == 0)
+                            continue;
+
+                        var aggregateResultItems = new List<ShopDefItem>();
+                        int collectionsPending = factoryItemCollections.Count;
+
+                        void singleCollectionCallback(ItemCollectionResult result)
+                        {
+                            if (result?.items != null)
+                            {
+                                aggregateResultItems.AddRange(result.items);
+                            }
+
+                            collectionsPending--;
+
+                            if (collectionsPending <= 0)
+                            {
+                                ProcessFactorySystem(system, aggregateResultItems, simGame);
+                            }
+                        }
+
+                        foreach (var collectionDef in factoryItemCollections)
+                        {
+                            //Main.Log.LogDebug($"Checking {collectionDef.ID} for {system.Name}.");
+                            simGame.ItemCollectionResultGen.GenerateItemCollection(collectionDef, -1, singleCollectionCallback);
+                        }
+                    }
+                }
+            }
+
+            private static List<ItemCollectionDef> GetFactoryItemCollections(SimGameState simGame, StarSystem system)
+            {
+                var factoryItemCollections = new HashSet<string>(
+                    system.Def.FactionShopItems?
+                        .Where(id => !string.IsNullOrEmpty(id) && id.StartsWith("itemCollection_factory"))
+                        .Select(id => id.Replace("factoryHolder", "factory")) ?? []
+                );
+
+                if (system.Name == "Outreach")
+                {
+                    factoryItemCollections.Remove("itemCollection_factory_Outreach");
+                    factoryItemCollections.Add("itemCollection_systemStores_Mechs_Dragoons");
+                    factoryItemCollections.Add("itemCollection_systemStores_Vehicles_Dragoons");
+
+                    if (simGame.CurrentDate >= new DateTime(3055, 1, 1) &&
+                        system.Def.SystemShopItems != null && system.Def.SystemShopItems.Contains("itemCollection_special_GAL"))
+                    {
+                        factoryItemCollections.Add("itemCollection_special_GAL");
+                    }
+                }
+                else if (system.Name == "New Valencia" && simGame.CurrentDate >= new DateTime(3030, 1, 1) &&
+                         system.Def.SystemShopItems != null && system.Def.SystemShopItems.Contains("itemCollection_factory_MarauderII"))
+                {
+                    factoryItemCollections.Add("itemCollection_factory_MarauderII");
+                }
+
+                var validCollections = factoryItemCollections
+                    .Where(simGame.DataManager.ItemCollectionDefs.Exists)
+                    .Select(simGame.DataManager.ItemCollectionDefs.Get)
+                    .ToList();
+
+                return validCollections;
+            }
+
+            private static void ProcessFactorySystem(StarSystem system, List<ShopDefItem> items, SimGameState simGame)
+            {
+                List<MechDef> mechDefs = [];
+                List<VehicleDef> vehicleDefs = [];
+
+                foreach (var item in items.Where(i => i.Type is ShopItemType.Mech))
+                {
+                    if (item.ID.StartsWith("mechdef_"))
+                    {
+                        if (!simGame.DataManager.MechDefs.TryGet(item.ID, out var mechDef)) continue;
+                        mechDefs.Add(mechDef);
+                    }
+                    else if (item.ID.StartsWith("vehicledef_"))
+                    {
+                        if (!simGame.DataManager.VehicleDefs.TryGet(item.ID, out var vehicleDef)) continue;
+                        vehicleDefs.Add(vehicleDef);
+                    }
+                }
+
+                var mechText = BuildProductionText(mechDefs, def => def.Chassis.weightClass, def => def.Name, def => def.Chassis.Tonnage);
+                var vehicleText = BuildProductionText(vehicleDefs, def => def.Chassis.weightClass, def => def.Description.Name, def => def.Chassis.Tonnage);
+
+                if (mechText.Any() || vehicleText.Any())
+                {
+                    AppendProductionText(system, mechText, vehicleText);
+                }
+            }
+
+            private static List<string> BuildProductionText<T>(IEnumerable<T> items, Func<T, WeightClass> weightClass, Func<T, string> name, Func<T, float> tonnage)
+            {
+                return [.. items.GroupBy(weightClass)
+                    .OrderByDescending(group => group.Key)
+                    .Select(group =>
+                    {
+                        var unitNames = group
+                            .OrderBy(name)
+                            .Select(def => CreateUnbreakableUnitString(name(def), tonnage(def)))
+                            .Distinct()
+                            .ToList();
+
+                        if (!unitNames.Any()) return string.Empty;
+
+                        string header = $"{ToProperCase(group.Key.ToString())}:";
+                        unitNames[0] = $"\u00A0\u00A0<b>{header}</b>\u00A0{unitNames[0]}";
+                        return string.Join(", ", unitNames);
+                    }).Where(s => !string.IsNullOrEmpty(s))];
+            }
+
+            private static void AppendProductionText(StarSystem system, List<string> mechText, List<string> vehicleText)
+            {
+                bool hasMechs = mechText.Any();
+                bool hasVehicles = vehicleText.Any();
+
+                var finalTextBuilder = new StringBuilder();
+                finalTextBuilder.AppendLine("\n<size=80%>---</size>");
+                finalTextBuilder.AppendLine("<b>LOCAL PRODUCTION:</b>");
+
+                if (hasMechs)
+                {
+                    if (hasVehicles) finalTextBuilder.AppendLine("\u00A0<b>Mechs:</b>");
+                    foreach (string line in mechText!)
+                    {
+                        string wrappedLine = RichTextWrapper.WrapLine(line, 54);
+                        finalTextBuilder.AppendLine(wrappedLine);
+                    }
+                }
+
+                if (hasVehicles)
+                {
+                    if (hasMechs) finalTextBuilder.AppendLine("\n\u00A0<b>Vehicles:</b>");
+                    foreach (string line in vehicleText!)
+                    {
+                        string wrappedLine = RichTextWrapper.WrapLine(line, 54);
+                        finalTextBuilder.AppendLine(wrappedLine);
+                    }
+                }
+
+                system.Def.Description.Details += finalTextBuilder.ToString();
+            }
+
+            private static string CreateUnbreakableUnitString(string name, float tonnage) => $"{name} ({tonnage}t)".Replace(' ', '\u00A0');
+
+            private static string ToProperCase(string s) => string.IsNullOrEmpty(s) ? s : char.ToUpper(s[0]) + s.Substring(1).ToLower();
+        }
+    }
+}
---
+++ src/Utils/RichTextWrapper.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NavigationComputer.Utils
+{
+    public static class RichTextWrapper
+    {
+        private static readonly Regex tagRegex = new("<.*?>", RegexOptions.Compiled);
+        private const int tabWidth = 4;
+
+        /// <summary>
+        /// Gets the visible display length of a string after stripping rich text tags.
+        /// </summary>
+        private static int GetVisibleLength(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return 0;
+            }
+
+            string expandedTabs = text.Replace("\t", new string(' ', tabWidth));
+            return tagRegex.Replace(expandedTabs, "").Length;
+        }
+
+        /// <summary>
+        /// Wraps a single line of text containing rich text tags to a max width, indenting wrapped lines with a tab.
+        /// </summary>
+        public static string WrapLine(string text, int maxLineLength)
+        {
+            if (string.IsNullOrEmpty(text) || GetVisibleLength(text) <= maxLineLength)
+            {
+                return text;
+            }
+
+            string[] words = text.Split(' ');
+            var wrappedText = new StringBuilder();
+            int currentLineLength = 0;
+
+            foreach (string word in words)
+            {
+                int wordVisibleLength = GetVisibleLength(word);
+
+                if (currentLineLength == 0 && wordVisibleLength > maxLineLength)
+                {
+                    wrappedText.Append(word);
+                    currentLineLength = wordVisibleLength;
+                    continue;
+                }
+
+                if (currentLineLength == 0)
+                {
+                    wrappedText.Append(word);
+                    currentLineLength = wordVisibleLength;
+                }
+                else if (currentLineLength + 1 + wordVisibleLength <= maxLineLength)
+                {
+                    wrappedText.Append(" ");
+                    wrappedText.Append(word);
+                    currentLineLength += 1 + wordVisibleLength;
+                }
+                else
+                {
+                    wrappedText.Append("\n\t");
+                    wrappedText.Append(word);
+                    currentLineLength = tabWidth + wordVisibleLength;
+                }
+            }
+            return wrappedText.ToString();
+        }
+    }
+}
